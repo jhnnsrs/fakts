dc5e2cd02fbb74baced137db12b66b2d7637eb7b
jhnnsrs <jhnnsrs@gmail.com>
2022-03-22 11:41:01 +0100

    test windows 2019

--- fakts/grants/endpoint.py
+++ fakts/grants/endpoint.py
@@ -5,8 +5,8 @@ from fakts.beacon import EndpointDiscovery, FaktsRetriever
 
 
 class EndpointGrant(FaktsGrant):
-    endpoint: FaktsEndpoint = Field(default_facotry=FaktsEndpoint)
-    retriever_protocol: FaktsRetriever = Field(default_facotry=FaktsRetriever)
+    endpoint: FaktsEndpoint = Field(default_factory=FaktsEndpoint)
+    retriever_protocol: FaktsRetriever = Field(default_factory=FaktsRetriever)
 
     async def aload(self, previous={}, **kwargs):
         return await self.retriever_protocol.aretrieve(self.endpoint, previous=previous)
